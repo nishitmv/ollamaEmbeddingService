e3a2b55667fb8d9d063af41ac044f4a0306fe69d
nishitmv <nishitmv@gmail.com>
2025-01-09 14:59:18 +0530

    generating and searching  Document Embeddings with Opensearch and LLama3.2

--- src/main/java/com/nitron/ollamaEmbeddingService/OllamaEmbeddingServiceApplication.java
+++ src/main/java/com/nitron/ollamaEmbeddingService/OllamaEmbeddingServiceApplication.java
@@ -1,13 +1,45 @@
 package com.nitron.ollamaEmbeddingService;
 
+import org.apache.http.HttpHost;
+import org.opensearch.client.json.jackson.JacksonJsonpMapper;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.rest_client.RestClientTransport;
+import org.opensearch.client.RestClient;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.embedding.TokenCountBatchingStrategy;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.opensearch.OpenSearchVectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URISyntaxException;
 
 @SpringBootApplication
+@Configuration
 public class OllamaEmbeddingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OllamaEmbeddingServiceApplication.class, args);
 	}
+/*	@Bean
+	public OpenSearchClient openSearchClient() throws URISyntaxException {
+		RestClient restClient = RestClient.builder(HttpHost.create("http://localhost:9200"))
+				.build();
+
+		return new OpenSearchClient(new RestClientTransport(
+				restClient, new JacksonJsonpMapper()));
+	}
+	@Bean
+	public VectorStore vectorStore(OpenSearchClient openSearchClient, EmbeddingModel embeddingModel) {
+		return OpenSearchVectorStore.builder(openSearchClient, embeddingModel)
+				.index("custom-index")                // Optional: defaults to "spring-ai-document-index"
+				.similarityFunction("l2")             // Optional: defaults to "cosinesimil"
+				.initializeSchema(true)               // Optional: defaults to false
+				.batchingStrategy(new TokenCountBatchingStrategy()) // Optional: defaults to TokenCountBatchingStrategy
+				.build();
+	}*/
+
 
 }
--- src/main/java/com/nitron/ollamaEmbeddingService/service/EmbeddingService.java
+++ src/main/java/com/nitron/ollamaEmbeddingService/service/EmbeddingService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -21,34 +20,50 @@ public class EmbeddingService {
         this.vectorStore = vectorStore;
     }
 
-    public void generateCodeEmbeddingsAndStore()
-    {
+    public void generateCodeEmbeddingsAndStore() {
         try {
             //String code1 = Files.readString(Paths.get("/home/nishit/Documents/JavaWorkspace/TransSearchService/src/main/java/com/nitron/TransSearchService/service/TranslationService.java"));
-           // String code2 = Files.readString(Paths.get("/home/nishit/Documents/JavaWorkspace/TransSearchService/src/main/java/com/nitron/TransSearchService/repository/TranslationsRepository.java"));
-           // String code3 = Files.readString(Paths.get("/home/nishit/Documents/JavaWorkspace/TransSearchService/src/main/java/com/nitron/TransSearchService/controller/TranslationsController.java"));
-           // var cleanString = code1.replaceAll("\n", "");
-           // var cleanString2 = code2.replaceAll("\n", "");
+            // String code2 = Files.readString(Paths.get("/home/nishit/Documents/JavaWorkspace/TransSearchService/src/main/java/com/nitron/TransSearchService/repository/TranslationsRepository.java"));
+            // String code3 = Files.readString(Paths.get("/home/nishit/Documents/JavaWorkspace/TransSearchService/src/main/java/com/nitron/TransSearchService/controller/TranslationsController.java"));
+            // var cleanString = code1.replaceAll("\n", "");
+            // var cleanString2 = code2.replaceAll("\n", "");
             String doc1 = Files.readString(Paths.get("/home/nishit/Documents/JavaWorkspace/ollamaEmbeddingService/src/main/resources/doc1.txt"));
             String doc2 = Files.readString(Paths.get("/home/nishit/Documents/JavaWorkspace/ollamaEmbeddingService/src/main/resources/doc2.txt"));
+            var len1 = doc1.length();
+            var len2 = doc2.length();
+            createEmbeddingAndWriteToDB(doc1, len1);
 
-
-            List <Document> documents = List.of(
-                    new Document(doc1)
-            );
-            vectorStore.add(documents);
-
-            List <Document> documents2 = List.of(
-                    new Document(doc2)
-            );
-            vectorStore.add(documents2);
-        } catch ( IOException e) {
+             createEmbeddingAndWriteToDB(doc2, len2);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public List<Document> searchByQuery(String query)
-    {
+    private void createEmbeddingAndWriteToDB(String doc1, int len) {
+        System.out.println("Started ");
+        var start = 0;
+        var end = 0;
+        while (len > 0) {
+            if (len > 1000) {
+                len -= 1000;
+                end+= 1000;
+            } else {
+                end+= len;
+                len = 0;
+            }
+            var str = doc1.substring(start, end);
+            List<Document> documents = List.of(
+                    new Document(str)
+            );
+            vectorStore.add(documents);
+            System.out.println("Completed Range "+start+" : "+end);
+
+            start+=1000;
+        }
+
+    }
+
+    public List<Document> searchByQuery(String query) {
         return vectorStore.similaritySearch(query);
     }
 
